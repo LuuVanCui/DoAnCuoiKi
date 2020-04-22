a599f1ca29918081cfd348b316e8312c1740d4df
trunghieu <hieuld007@gmail.com>
2020-04-22 21:42:26 +0700

    them form setup

---
+++ QuanLyNhaXe01/Caculate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaXe01
+{
+    class Caculate
+    {
+        MyDB mydb = new MyDB();
+        public DataTable getData(SqlCommand cmd)
+        {
+            cmd.Connection = mydb.getConnection;
+       
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            return table;
+        }
+
+       /* public DataTable insertMoney(SqlCommand cmd)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            
+            return table;
+        }*/
+    }
+}
